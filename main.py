db510fb2d167e30fd6dedc5ae802633361c3e984
BOVLI TAWFIK <72839896+Coding-man12435676543@users.noreply.github.com>
2021-06-23 01:02:36 +0000

    Added a feature where users can now add new encouraging messages and delete them too, with replt.it database.

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
-import discord, requests, os, json
+import discord, requests, os, json, random
 from googlesearch import search
+from replit import db
 
 client = discord.Client()
+
+sad_words = ['sad', 'depressed', 'unhappy', 'angry', 'miserable', 'depressing']
+
+starter_encouragements = [
+  'Cheer up!',
+  'Hang in there.',
+  'You are a great person / bot!'
+]
 
 def get_quote():
   response = requests.get('https://zenquotes.io/api/random')
   json_data = json.loads(response.text)
   quote = json_data[0]['q'] + ' -{}'.format(json_data[0]['a'])
   return(quote)
+
+def update_encouragements(encouraging_message):
+  if 'encouragements' in db.keys():
+    encouragements = db['encouragements']
+    encouragements.append(encouraging_message)
+    db['encouragements'] = encouragements
+  else:
+    db['encouragements'] = [encouraging_message]
+
+def delete_encouragement(index):
+  encouragements = db['encouragements']
+  if len(encouragements) > index:
+    del encouragements[index]
+  db['encouragements'] = encouragements
 
 @client.event
 async def on_ready():
@@ -18,9 +41,31 @@ async def on_ready():
 async def on_message(message):
   if message.author == client.user:
     return
+
+  msg = message.content
   
-  if message.content.startswith('$inspire'):
+  if msg.startswith('$inspire'):
     quote = get_quote()
     await message.channel.send(quote)
+
+  options = starter_encouragements
+  if 'encouragements' in db.keys():
+    options = options + list(db['encouragements'])
+  
+  if any(word in msg for word in sad_words):
+    await message.channel.send(random.choice(options))
+
+  if msg.startswith('$new'):
+    encouraging_message = msg.split('$new ', 1)[1]
+    update_encouragements(encouraging_message)
+    await message.channel.send('New encouraging message added.')
+
+  if msg.startswith('$del'):
+    encouragements = []
+    if 'encouragements' in db.keys():
+      index = int(msg.split('$del', 1)[1])
+      delete_encouragement(index)
+      encouragements = db['encouragements']
+    await message.channel.send(encouragements)
 
 client.run(os.getenv('TOKEN'))
